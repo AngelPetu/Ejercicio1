e9bee344ca12b7d57f2b57318b7759cc864614e9
AngelPetu <aepetunchi@gmail.com>
2015-09-28 21:00:16 -0300

    Create PrincipalTest.java

---
+++ test/helloWorld/PrincipalTest.java
@@ -0,0 +1,14 @@
+public class Principal {
+public static void main(String[] args) {
+	
+	Cronometro Crono1 = new Cronometro();
+	
+	for (int i = 0; i < 5000; i++) {
+		Crono1.IncrementarTiempo();
+	}
+	Crono1.MostrarTiempo();
+	Crono1.Reiniciar();
+	Crono1.MostrarTiempo();
+	
+}
+}
